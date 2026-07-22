9f9d92499e8b2d32c30457c03838e246207a871b
rma500 <rma200@scarletmail.rutgers.edu>
2026-07-22 10:48:55 -0400

    Make Email field unique in schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE Customer (
     Password VARCHAR(80) NOT NULL,
     FirstName VARCHAR(80) NOT NULL,
     LastName VARCHAR(80) NOT NULL,
-    Email VARCHAR(100) NOT NULL
+    Email VARCHAR(100) NOT NULL UNIQUE
     
 );
 
